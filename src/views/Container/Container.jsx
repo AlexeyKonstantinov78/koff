b1da9012ad85c7eaca15a84308d78b791b7c148b
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2024-01-11 10:51:38 +0300

    update container

--- src/views/Container/Container.jsx
+++ src/views/Container/Container.jsx
@@ -1,10 +1,11 @@
 import _ from "./Container.module.scss";
 
-export const Container = ({ children, className }) => className ? (
+export const Container = ({ children, className }) =>
+(className ? (
   <div className={`${_.container} ${className}`}>{children}</div>
 ) : (
   <div className={_.container}>{children}</div>
-);
+));
 
 // export const Container = ({ children, className = "" }) => (
 //   <div className={_.container + " " + className}>{children}</div>
