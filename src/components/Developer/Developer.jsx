4fd0f2b5c06ccd4cdc52d8e3056ef4a7c167a9fb
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2024-01-14 19:49:28 +0300

    feat add footer components and style

--- src/components/Developer/Developer.jsx
+++ src/components/Developer/Developer.jsx
@@ -1,5 +1,26 @@
 import _ from "./Developer.module.scss";
 
 export const Developer = () => (
-  <div className={_.developer_list}>Developer</div>
+  <ul className={_.developers}>
+    <li className={_.item}>
+      Designer:&nbsp;
+      <a
+        className={_.link}
+        href="https://t.me/Mrshmallowww"
+        target="_blank"
+        rel="noreferrer">
+        Anastasia Ilina
+      </a>
+    </li>
+    <li className={_.item}>
+      Designer:&nbsp;
+      <a
+        className={_.link}
+        href="https://t.me/Alex_kav"
+        target="_blank"
+        rel="noreferrer">
+        Aleksey Konstantinov
+      </a>
+    </li>
+  </ul>
 );
