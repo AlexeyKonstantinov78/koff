cad4d26221ccb7e2f7521ff12413bb3bd65559fb
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2024-01-14 17:11:00 +0300

    feat update logo and searchform

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,10 +2,11 @@ import _ from "./Logo.module.scss";
 import logoSvg from "./img/logo.svg";
 
 export const Logo = () => (
-  <a href="/">
+  <a className={_.link} href="/">
     <img
       className={_.logo}
       src={logoSvg}
-      alt="Логотип мебельного маркета Koff" />
+      alt="Логотип мебельного маркета Koff"
+    />
   </a>
 );
--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -3,7 +3,7 @@ import { Container } from "../Container/Container.jsx";
 export const Main = () => (
   <main>
     <Container>
-      <h1>Hello</h1>
+      <h1>Hello main view component</h1>
     </Container>
   </main>
 );
