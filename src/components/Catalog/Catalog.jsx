185da33641bf25939ed59cbe9de3923fb32d9ac5
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-01-15 19:42:47 +0300

    feat add redux category fetch data

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -2,35 +2,16 @@ import { Container } from "../../views/Container/Container";
 import { CategoryItem } from "../CategoryItem/CategoryItem";
 import _ from "./Catalog.module.scss";
 
-export const Catalog = () => (
+export const Catalog = ({ data }) => (
   <nav className={_.catalog}>
     <Container>
       <h2 className={`${_.title} visually-hidden`}>Категории товаров</h2>
       <ul className={_.catalog__list}>
-        <li className="_.catalog__item">
-          <CategoryItem title="Тумбы" />
-        </li>
-        <li className="_.catalog__item">
-          <CategoryItem title="Полки" />
-        </li>
-        <li className="_.catalog__item">
-          <CategoryItem title="Стулья" />
-        </li>
-        <li className="_.catalog__item">
-          <CategoryItem title="Столы" />
-        </li>
-        <li className="_.catalog__item">
-          <CategoryItem title="Пуфы и банкетки" />
-        </li>
-        <li className="_.catalog__item">
-          <CategoryItem title="Кровати" />
-        </li>
-        <li className="_.catalog__item">
-          <CategoryItem title="Диваны" />
-        </li>
-        <li className="_.catalog__item">
-          <CategoryItem title="Стеллажи" />
-        </li>
+        {data.map((item, i) => (
+          <li key={i} className="_.catalog__item">
+            <CategoryItem title={item} />
+          </li>
+        ))}
       </ul>
     </Container>
   </nav>
